167edbe684284c213d4b7da1851dac851ab9366b
drizzle66 <alexanderbolaji66@gmail.com>
2022-09-20 01:22:06 +0100

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,16 @@
 
 /**
  * main - check the code
+ * swap_int - swaps the values of two integers
  * @a: integer 1
  * @b: integer 2
  * Return: Always 0.
  */
 
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
 	int c = *a;
+	
 	*a = *b;
 	*b = c;
 }
