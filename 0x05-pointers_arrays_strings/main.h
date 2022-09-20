6db310154bacdbbc903d4520122af8a618e057e1
drizzle66 <alexanderbolaji66@gmail.com>
2022-09-20 02:45:15 +0100

    done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ *char prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +19,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /*MAIN_H*/
+
